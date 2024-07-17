1418072bac6cf0d710024bbf355908c0ebb58d03
crikkk123 <149687686+crikkk123@users.noreply.github.com>
2024-07-17 21:13:01 +0800

    Add files via upload

---
+++ 2024\07\17/card.cpp
@@ -0,0 +1,56 @@
+#include "card.h"
+
+Card::Card()
+{
+
+}
+
+void Card::setSuit(CardSuit suit){
+    m_suit = suit;
+}
+
+void Card::setPoint(CardPoint point){
+    m_point = point;
+}
+
+Card::CardSuit Card::suit() const{
+    return m_suit;
+}
+
+Card::CardPoint Card::point() const{
+    return m_point;
+}
+
+bool lessSort(const Card &c1, const Card &c2)
+{
+    if(c1.point() == c2.point())
+    {
+        return c1.suit()<c2.suit();
+    }
+    else
+    {
+        return c1.point()<c2.point();
+    }
+}
+
+bool greaterSort(const Card& c1,const Card& c2)
+{
+    if(c1.point() == c2.point())
+    {
+        return c1.suit()>c2.suit();
+    }
+    else
+    {
+        return c1.point()>c2.point();
+    }
+}
+
+bool operator == (const Card& left,const Card& right)
+{
+    return (left.point()==right.point() && left.suit()==right.suit());
+}
+
+uint qHash(const Card &card)
+{
+    return card.point()*100+card.suit();
+}
