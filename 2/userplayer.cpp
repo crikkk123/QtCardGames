30b34ba951276f22edb70e2fab067c100bc1f0e6
crikkk123 <149687686+crikkk123@users.noreply.github.com>
2024-07-18 21:06:15 +0800

    Add files via upload

---
+++ 2/userplayer.cpp
@@ -0,0 +1,18 @@
+#include "userplayer.h"
+
+
+
+UserPlayer::UserPlayer(QObject *parent)
+{
+    m_type = Player::User;
+}
+
+void UserPlayer::prepareCallLord()
+{
+
+}
+
+void UserPlayer::preparePlayHand()
+{
+
+}
